a3a84a1da7b030af114f71f530987f309e4486ad
antoniopucciarelli <antonio.pucciarelli@gmail.com>
2022-04-04 23:46:52 +0200

    update with function that maps the main parameters for the chosing of the initial design parameter

---
+++ turboCoeff/coeff.py
@@ -0,0 +1,236 @@
+# TURBOMACHINERY -- ENGINEERING DIMENSION AND COEFFICIENT FUNCTIONS LIBRARY
+# AUTHOR: antonio pucciarelli 
+#
+# PROGRAM DESCRIPTION
+#   TURBOMACHINERY: compressor/turbine
+#   CONTENT: engineering coefficient functions for the computation of turbomachinery performace
+#  
+
+# WORK COMPUTATION 
+
+def L_is(Tin=0, Pin=0, Pout=0, beta=0, kind='', gamma=1.4, R=287.06):
+    '''
+    Compressor work computation
+        working conditions: isentropic transformation
+
+        function inputs:
+            Tin         [K]         -- initial temperature 
+            Pin         [Pa]        -- initial pressure
+            Pout        [Pa]        -- final pressure 
+            beta        [--]        -- pressure ratio 
+            kind                    -- turbomachinery type 
+                                    -- values:
+                                        -- compressor
+                                        -- turbine 
+            gamma       [--]        -- gas heat capacity ratio
+            R = 287.06  [J/kg K]    -- air gas constant Ru/Mair 
+    '''
+
+    # there are 2 ways for L computation
+    # 1. using directly beta
+    # 2. computing directly beta from Pin and Pout 
+    if Pin != 0 and Pout !=0: 
+        if kind == 'compressor':
+            beta = Pout/Pin
+        elif kind == 'turbine':
+            beta = Pin/Pout 
+
+    # work computation
+    if kind == 'compressor':
+        Lis = (gamma * R)/(gamma - 1) * Tin * (beta**((gamma - 1)/gamma) - 1)
+    elif kind == 'turbine':
+        Lis = (gamma * R)/(gamma - 1) * Tin * (1 - beta**((1 - gamma)/gamma))
+
+    return Lis
+
+def L_poly(Tin=0, Pin=0, Pout=0, beta=0, kind='', n=0, R=287.06):
+    '''
+    Compressor work computation
+        working conditions: polytropic transformation
+
+        function inputs:
+            Tin         [K]         -- initial temperature 
+            Pin         [Pa]        -- initial pressure
+            Pout        [Pa]        -- final pressure 
+            beta        [--]        -- pressure ratio 
+            kind                    -- turbomachinery type 
+                                    -- values:
+                                        -- compressor
+                                        -- turbine 
+            n           [--]        -- gas polytropic index
+            R = 287.06  [J/kg K]    -- air gas constant Ru/Mair 
+    '''
+
+    # there are 2 ways for L computation
+    # 1. using directly beta
+    # 2. computing directly beta from Pin and Pout 
+    if Pin != 0 and Pout !=0: 
+        if kind == 'compressor':
+            beta = Pout/Pin
+        elif kind == 'turbine':
+            beta = Pin/Pout 
+
+    # work computation
+    if kind == 'compressor':
+        Lpoly = (n * R * Tin)/(n - 1) * (beta**((n - 1)/n) - 1) 
+    elif kind == 'turbine':
+        Lpoly = (n * R * Tin)/(n - 1) * (1 - beta**((1 - n)/n))
+    
+    return Lpoly
+
+def L_adR(Tin=0, Pin=0, Pout=0, n=0, beta=0, kind='', gamma=1.4, R=287.06):
+    '''
+    Compressor work computation
+        working conditions: polytropic transformation
+
+        function inputs:
+            Tin         -- initial temperature 
+            Pin         -- initial pressure
+            Pout        -- final pressure 
+            beta        -- pressure ratio 
+            kind        -- turbomachinery type 
+                        -- values:
+                            -- compressor
+                            -- turbine 
+            n           -- polytropic index
+            gamma       -- gas heat capacity ratio 
+            R = 287.06  -- air gas constant Ru/Mair 
+    '''
+
+    # there are 2 ways for L computation
+    # 1. using directly beta
+    # 2. computing directly beta from Pin and Pout 
+    if Pin != 0 and Pout !=0: 
+        if kind == 'compressor':
+            beta = Pout/Pin
+        elif kind == 'turbine':
+            beta = Pin/Pout 
+
+    # work computation
+    if kind == 'compressor':
+        LadR = (gamma * R * Tin)/(gamma - 1) * (beta**((n - 1)/n) - 1)
+    elif kind == 'turbine':
+        LadR = (gamma * R * Tin)/(gamma - 1) * (1 - beta**((1 - n)/n))
+    
+    return LadR
+
+# EFFICIENCY COMPUTATION
+
+def ise_eff(hout_is=0, hin=0, hout=0, Tout_is=0, Tin=0, Tout=0, beta=0, kind='', gamma=0, n=0, L_is=0, L_adR=0):
+    '''
+    Isentropic efficiency computation
+        function inputs:
+            hout_is     -- final enthalpy for an isentropic transformation
+            hin         -- initial enthaply 
+            hout        -- final enthalpy for the real transformation
+            Tout_is     -- final temperature for an isentropic transformation
+            Tin         -- initial temperature
+            Tout        -- final temperature for the real transformation
+            beta        -- pressure ratio
+            kind        -- turbomachinery type 
+                        -- values:
+                            -- compressor
+                            -- turbine 
+            gamma       -- gas heat capacity ratio
+            n           -- polytropic index
+            L_is        -- isentropic compressor work
+            L_adR       -- adiabatic real compressor work 
+        
+        computation -- there are different ways for the computation of the efficiency
+            enthalpy based 
+            temperature based 
+            pressure ratio based 
+            work based
+    '''
+
+    if kind == 'compressor':
+        # enthalpy based computation
+        if hout_is != 0 and hin != 0 and hout != 0:
+            eta = (hout_is - hin) / (hout - hin)
+
+        # temperature based 
+        if Tout_is != 0 and Tin != 0 and Tout != 0:
+            eta = (Tout_is - Tin) / (Tout - Tin)
+
+        # pressure ratio based 
+        if beta != 0 and gamma != 0 and n != 0: 
+            eta = (beta**((gamma - 1)/gamma) - 1) / (beta**((n - 1)/n) - 1)
+
+        # work based 
+        if L_is != 0 and L_adR != 0:
+            eta = L_is / L_adR 
+   
+    elif kind == 'turbine':
+        # enthalpy based computation
+        if hout_is != 0 and hin != 0 and hout != 0:
+            eta = (hout - hin) / (hout_is - hin)
+
+        # temperature based 
+        if Tout_is != 0 and Tin != 0 and Tout != 0:
+            eta = (Tout - Tin) / (Tout_is - Tin)
+
+        # pressure ratio based 
+        if beta != 0 and gamma != 0 and n != 0: 
+            eta = (1 - beta**((1 - n)/n)) / (1 - beta**((1 - gamma)/gamma))
+
+        # work based 
+        if L_is != 0 and L_adR != 0:
+            eta = L_adR / L_is 
+
+    return eta    
+
+def poly_eff(gamma=0, n=0, L_p=0, L_adR=0, kind=''):
+    '''
+    Polytropic efficiency computation
+        function inputs:
+            gamma       -- gas heat capacity ratio
+            n           -- polytropic index
+            L_p         -- polytropic transformation compressor work
+            L_adR       -- adiabatic real compressor work 
+            kind        -- turbomachinery type 
+                        -- values:
+                            -- compressor
+                            -- turbine 
+        
+        computation -- there are different ways for the computation of the efficiency
+            work based
+            transformation factor based 
+    '''
+
+    if kind == 'compressor':
+        # work based 
+        if L_p != 0 and L_adR != 0:
+            eta = L_p / L_adR 
+
+        # transformation factor based
+        if gamma != 0 and n != 0:
+            eta = (n * (gamma - 1)) / (gamma * (n - 1))
+    elif kind == 'turbine':
+        # work based 
+        if L_p != 0 and L_adR != 0:
+            eta = L_adR / L_p
+
+        # transformation factor based
+        if gamma != 0 and n != 0:
+            eta = (gamma * (n - 1)) / (n * (gamma - 1))
+
+    return eta    
+
+def reHeat_factor(eta_poly, eta_is, kind=''):
+    '''
+    Re-heat factor computation
+        function inputs:
+            eta_poly    -- polytropic transformation efficiency
+            eta_is      -- insentropic trnasformation efficiency
+            kind        -- turbomachinery type 
+                        -- values:
+                            -- compressor
+                            -- turbine 
+    '''
+
+    if kind == 'compressor':
+        reHeatFactor = eta_poly / eta_is - 1
+    elif kind == 'turbine':
+        reHeatFactor = eta_is / eta_poly - 1
+    
+    return reHeatFactor
--- turboCoeff/similarity.py
+++ turboCoeff/similarity.py
@@ -7,6 +7,7 @@
 #  
 
 # importing libraries
+import enum
 import numpy as np 
 import matplotlib.pyplot as plt 
 from scipy import interpolate
@@ -70,7 +71,7 @@ def efficiency(phi=0, rD=0, plot=False, save=False, position='efficiency.pgf'):
     if plot:
 
         plt.figure(figsize=(9,9))
-        plt.title('Lieblein efficiency -- axial compressor')
+        plt.title('Lieblein efficiency\nAXIAL COMPRESSOR')
 
         # plotting efficiency taken from charts 
         plt.plot(0.48, 0.5, 'ok', label=r'$\eta = 0.926$')
@@ -104,8 +105,8 @@ def efficiency(phi=0, rD=0, plot=False, save=False, position='efficiency.pgf'):
             'pgf.rcfonts': False,
         })
 
-        plt.figure(figsize=(8,8))
-        plt.title('Lieblein efficiency -- axial compressor')
+        plt.figure(figsize=(9,9))
+        plt.title('Lieblein efficiency\nAXIAL COMPRESSOR')
 
         # plotting efficiency taken from charts 
         plt.plot(0.48, 0.5, 'ok', label=r'$\eta = 0.926$')
@@ -133,7 +134,7 @@ def efficiency(phi=0, rD=0, plot=False, save=False, position='efficiency.pgf'):
     if rD != 0 and phi != 0:
         return eta
 
-def stagePerf(phi=0, psi=0, perc=0, rD=0.5, phiVec=np.linspace(0,1.1,1000), plot=True, save=False, position='stagePerf.pgf'):
+def stagePerf(phi=0, psi=0, perc=1, rD=0.5, phiVec=np.linspace(0,1.5,1000), plot=True, save=False, position='stagePerf.pgf'):
     '''
     This function plots the phi and psi parameter with respect to ASME axial compressor design constraints
         The main constraints for the study of the adimensional parameters are:
@@ -164,35 +165,48 @@ def stagePerf(phi=0, psi=0, perc=0, rD=0.5, phiVec=np.linspace(0,1.1,1000), plot
     psiLim = lambda phi: 6 * Rcap / 17 + 0.85 * (0.5/Rcap)**1.18 * phi**(2 + 0.1/Rcap)
     psiBeta = lambda phi: 5 * phi - 2 * rD
 
-    def phiLim(phi, error=5e-3):
+    def phiFinder(phiVec, psiTarget, perc=perc, error=5e-3):
         '''
-        This function computes the phi value for which psiLim == 1
+        This function computes the phi value for a defined psi value.
         '''
-        for _,ii in enumerate(phi):
-            if np.abs(( psiLim(ii) - 1 ) / psiLim(ii)) < error:
-                return ii 
+
+        if perc != 0:
+            for _,phi in enumerate(phiVec):
+                if np.abs((perc * psiLim(phi) - psiTarget) / (perc * psiLim(phi))) < error:
+                    return phi 
 
     # values computation
-    psiLIM = psiLim(phiVec)    # work coefficient limit vs flow coefficient 
-    psiBETA = psiBeta(phiVec)  # work coefficient vs flow coefficient for beta = 70deg 
+    psiLIM = psiLim(phiVec)             # work coefficient limit vs flow coefficient 
+    psiBETA = psiBeta(phiVec)           # work coefficient vs flow coefficient for beta = 70deg 
+    phi1 = phiFinder(phiVec, 1, perc=1) # phi value such that psiLim == 1
 
     # computing psi with respect to a percentage and phi 
-    if perc != 0 and phi != 0 and psi == 0: 
+    if perc != 0 and phi != 0 and psi == 0:      
+        # computing psi 
         psi = perc * psiLim(phi)
+
+    # computing phi such that psi == psiTarget
+    if phi == 0 and psi != 0:
+        phi = phiFinder(phiVec, psi)
 
     if plot:
         # plotting
-        plt.figure()
+        plt.figure(figsize=(8,8))
         plt.plot(phiVec, psiLIM, 'k', label=r'$\psi_{Lim}$')
         plt.plot(phiVec, psiBETA, 'r', label=r'$\beta > 70^{\circ}$')
-        plt.plot([phiLim(phiVec),phiLim(phiVec)] , [0, np.max(psiLIM)], 'b', label=r'$\phi_{Lim}$')
+        plt.plot([phi1, phi1] , [0, np.max(psiLIM)], 'b', label=r'$\phi_{Lim}$')
         if psi != 0 and phi !=0:
-            plt.plot(phi, psi, 'og', label=r'$[\phi = {0:.3f}, \psi = {1:.3f}]$'.format(phi, psi))
-        plt.xlim(0,np.max(phiVec))
+            plt.plot(phi, psi, linestyle='', marker='o', markersize=7, markeredgewidth=1, markeredgecolor='k', color='g', label=r'$[\phi = {0:.3f}, \psi = {1:.3f}]$'.format(phi, psi))
+        plt.xlim(0,phi1)
         plt.ylim(0,1)
         plt.xlabel(r'$\phi$')
         plt.ylabel(r'$\psi$')
-        plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD))
+        plt.xticks(np.arange(0,phi1+0.1,0.1))
+        plt.yticks(np.arange(0,1.1,0.1))
+        if perc == 0:
+            plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD))
+        else:
+            plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD) + '    ' + r'$Percentage = {0:d} \%$'.format(int(perc*100)))
         plt.grid(linestyle='--')
         plt.legend(loc='upper left', bbox_to_anchor=(0, 1))
         plt.tight_layout()
@@ -209,23 +223,293 @@ def stagePerf(phi=0, psi=0, perc=0, rD=0.5, phiVec=np.linspace(0,1.1,1000), plot
             'pgf.rcfonts': False,
         })
         # plotting 
-        plt.figure()
+        plt.figure(figsize=(8,8))
         plt.plot(phiVec, psiLIM, 'k', label=r'$\psi_{Lim}$')
         plt.plot(phiVec, psiBETA, 'r', label=r'$\beta > 70^{\circ}$')
-        plt.plot([phiLim(phiVec),phiLim(phiVec)] , [0, np.max(psiLIM)], 'b', label=r'$\phi_{Lim}$')
+        plt.plot([phi1, phi1] , [0, np.max(psiLIM)], 'b', label=r'$\phi_{Lim}$')
         if psi != 0 and phi !=0:
             plt.plot(phi, psi, 'og', label=r'$[\phi = {0:.3f}, \psi = {1:.3f}]$'.format(phi, psi))
         plt.xlim(0,np.max(phiVec))
         plt.ylim(0,1)
         plt.xlabel(r'$\phi$')
         plt.ylabel(r'$\psi$')
-        plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD))
+        plt.xticks(np.arange(0,np.max(phiVec)+0.1,0.1))
+        plt.yticks(np.arange(0,1.1,0.1))
+        if perc == 0:
+            plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD))
+        else:
+            plt.title(r'$\frac{W_2}{W_1} > 0.7$' + '    ' + r'$\chi = {0:.2f}$'.format(rD) + '    ' + r'$Percentage = {0:d} \%$'.format(int(perc*100)))
         plt.grid(linestyle='--')
         plt.legend(loc='upper left', bbox_to_anchor=(0, 1))
         plt.tight_layout()
         # figure saving 
         plt.savefig(position, bbox_inches='tight')
 
-    if perc != 0 and phi != 0 and psi !=0:
-        return psi 
+    if phi != 0 and psi !=0:
+        return phi, psi 
 
+def stageStudy(mFlux, betaP, rMean, Pt0, Tt0):
+    '''
+    This function allows to understand the behaviour of the stage with respect to different changes:
+        -- reaction degree rD
+        -- Vt / Umean 
+
+        inputs:
+            mFlux   -- mass flux 
+            betaP   -- total pressure ratio
+            rMean   -- mean radius 
+            Pt0     -- initial total pressure 
+            Tt0     -- initial total temperature 
+    '''
+
+    # importing libraries 
+    import coeff 
+
+    # air properties allocation 
+    R     = 287.06                  # air gas constant Ru / Mm      [J/kg K]
+    gamma = 1.4                     # specific heat ratio           [--]
+    cP    = gamma / (gamma - 1) * R # specific heat ratio @ P cost  [J/kg K]
+    
+    # vector allocation for the properties study 
+    rDarray = np.linspace(0.45, 0.65, 12)
+    Vt0UmeanArray = np.linspace(0.05, 0.35, 20)
+
+    fig0, [[ax0, ax1, ax2, ax3], [ax4, ax5, ax6, ax7]] = plt.subplots(figsize=(20,8), nrows=2, ncols=4)
+
+    for _,rD in enumerate(rDarray):
+
+        # vector allocation 
+        Vt0UmeanVec = np.array([])
+        phiVec = np.array([])
+        psiVec = np.array([])
+        etaVec = np.array([])
+        alpha0Vec = np.array([])
+        UmeanVec = np.array([])
+        rD_etaVec = np.array([])
+        Mr0Vec = np.array([])
+        b0Vec = np.array([])
+        b1Vec = np.array([])
+        b2Vec = np.array([])
+
+        for _,Vt0Umean in enumerate(Vt0UmeanArray):
+            # Vt0 = (1 - rD - lam/4) * Umean -> lam = 4 * (1 - rD - Vt0/Umean) 
+            # psi = lam / 2 
+            lam = (1 - rD - Vt0Umean) * 4 
+            psiTarget = lam / 2
+
+            # plotting charts 
+            try:
+                phi, psi = stagePerf(psi=psiTarget, rD=rD, plot=False, perc=0.97)
+                eta = efficiency(phi=phi, rD=rD, plot=False)
+
+                # compute flow properties 
+                # WORK
+                # ideal compression work 
+                Lis = coeff.L_is(Tin=Tt0, beta=betaP, gamma=gamma, kind='compressor')
+                # real compression work 
+                L = Lis / eta
+                # total temperature computation
+                Tt1 = L / cP + Tt0
+
+                # ROTATION 
+                # mean section revolution speed from work coefficient
+                Umean = np.sqrt(L / psi) 
+                # angular speed 
+                omega = Umean / rMean
+                # rpm 
+                n = omega * 60 / (2 * np.pi)
+
+                # MAIN VELOCITIES COMPUTATION FROM WORK RESULTS
+                # Va0, Vt0 computation 
+                # axial inlet velocity from flow coefficient
+                Va0 = phi * Umean
+                # tangential inlet velocity from reaction degree and work coefficient
+                Vt0 = (1 - rD - lam/4) * Umean 
+                # under Umean assumption & Vt0 assumption
+                Vt1 = (1 - rD + lam/4) * Umean 
+                Vtinf = (Vt1 + Vt0)/2
+
+                # ROTOR INLET QUANTITIES 
+                # main inlet quantities computation
+                # kinetics 
+                # relative speed computation 
+                Wa0 = Va0
+                Wt0 = Vt0 - Umean
+                # veolcity magnitude computation
+                W0 = np.sqrt(Wa0**2 + Wt0**2)
+                V0 = np.sqrt(Va0**2 + Vt0**2)
+                # aerodynamic angles computation 
+                alpha0 = np.rad2deg(np.arctan(Vt0/Va0))
+                beta0 = np.rad2deg(np.arctan(Wt0/Wa0))
+                # thermodynamics
+                # static temperature computation
+                T0 = Tt0 - V0**2 / (2*cP)
+                # speed of sound computation
+                a0 = np.sqrt(gamma * R * T0)
+                # mach computation 
+                M0 = V0 / a0
+                Mr0 = W0 / a0
+                # static pressure computation
+                P0 = Pt0 / (1 + (gamma - 1)/2 * M0**2)**(gamma/(gamma-1))
+                # density computation 
+                rhot0 = Pt0 / (R * Tt0)
+                rho0 = rhot0 / (1 + (gamma - 1)/2 * M0**2)**(1/(gamma-1))
+
+                # ROTOR OUTLET/STATOR INLET QUANTITIES  
+                # outlet quantities 
+                Va1 = Va0 
+                Wa1 = Va1 
+                Wt1 = Vt1 - Umean
+                # velocity magnitude computation
+                V1 = np.sqrt(Va1**2 + Vt1**2)
+                W1 = np.sqrt(Wa1**2 + Wt1**2)
+                # flow angle computation
+                alpha1 = np.rad2deg(np.arctan(Vt1/Va1))
+                beta1 = np.rad2deg(np.arctan(Wt1/Wa1))
+                # thermodynamics
+                # static temperature computation
+                T1 = Tt1 - V1**2 / (2*cP)
+                # speed of sound computation 
+                a1 = np.sqrt(gamma * R * T1)
+                # mach number computation 
+                M1 = V1 / a1
+                Mr1 = W1 / a1
+                # pressure computation
+                P1 = P0 * (T1/T0)**(gamma/(gamma-1))
+                Pt1 = P1 * (Tt1/T1)**(gamma/(gamma-1))
+                # density computation
+                rhot1 = Pt1 / (R * Tt1)
+                rho1 = rhot1 / (1 + (gamma - 1)/2 * M1**2)**(1/(gamma-1))
+
+                # STATOR OUTLET QUANTITIES 
+                # main outlet quantities 
+                Va2 = Va1 
+                Pt2 = Pt1
+                Tt2 = Tt1
+                deltaH = (1 - rD) * L
+                T2 = deltaH / cP + T1 
+                V2 = np.sqrt(2 * cP * (Tt2 - T2))
+                Vt2 = np.sqrt(V2**2 - Va2**2)
+                alpha2 = np.rad2deg(np.arctan2(Vt2,Va2))
+                rhot2 = Pt2 / (R * Tt2)
+                # thermodynamics
+                a2 = np.sqrt(gamma * R * T2)
+                M2 = V2 / a2
+                P2 = P1 * (T2/T1)**(gamma/(gamma-1))
+                rho2 = rhot2 / (1 + (gamma - 1)/2 * M2**2)**(1/(gamma-1))
+
+                # BLADE RADIUS
+                # rotor inlet blade height 
+                b0 = mFlux / (rho0 * 2 * np.pi * rMean * Va0)
+                # rotor outlet/stator inlet  blade height
+                b1 = mFlux / (rho1 * 2 * np.pi * rMean * Va1)
+                # stator outlet blade height
+                b2 = mFlux / (rho2 * 2 * np.pi * rMean * Va2)
+
+                # reaction degree with efficiency 
+                rD_eta = cP * (T1 - T0) / L 
+
+                # vector 
+                Vt0UmeanVec = np.append(Vt0UmeanVec, Vt0Umean)
+                phiVec = np.append(phiVec, phi)
+                psiVec = np.append(psiVec, psi)
+                etaVec = np.append(etaVec, eta)
+                alpha0Vec = np.append(alpha0Vec, alpha0)
+                UmeanVec = np.append(UmeanVec, Umean)
+                rD_etaVec = np.append(rD_etaVec, rD_eta)
+                Mr0Vec = np.append(Mr0Vec, Mr0)
+                b0Vec = np.append(b0Vec, b0)
+                b1Vec = np.append(b1Vec, b1)
+                b2Vec = np.append(b2Vec, b2)
+
+            except:
+                pass
+
+        ax0.plot(Vt0UmeanVec, phiVec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax1.plot(Vt0UmeanVec, psiVec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax2.plot(Vt0UmeanVec, etaVec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax3.plot(Vt0UmeanVec, alpha0Vec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax4.plot(Vt0UmeanVec, UmeanVec, label=r'$\chi = {0:.2f}$'.format(rD))
+        #ax4.plot(Vt0UmeanVec, rD_etaVec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax5.plot(Vt0UmeanVec, Mr0Vec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax6.plot(Vt0UmeanVec, b0Vec, label=r'$\chi = {0:.2f}$'.format(rD))
+        ax7.plot(Vt0UmeanVec, b0Vec/2 + rMean, label=r'$\chi = {0:.2f}$'.format(rD))
+    
+    # ax0 setup
+    ax0.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax0.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax0.set_ylabel(r'$\phi$')
+    ax0.grid(linestyle='--')
+    ax0.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax1 setup 
+    ax1.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax1.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax1.set_ylabel(r'$\psi$')
+    ax1.grid(linestyle='--')
+    ax1.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax2 setup
+    ax2.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax2.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax2.set_ylabel(r'$\eta$')
+    ax2.grid(linestyle='--')
+    ax2.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax3 setup
+    ax3.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax3.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax3.set_ylabel(r'$\alpha_0$')
+    ax3.grid(linestyle='--')
+    ax3.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax4 setup
+    ax4.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax4.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax4.set_ylabel(r'$U_{{mean}}$')
+    ax4.grid(linestyle='--')
+    ax4.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax5 setup
+    ax5.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax5.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax5.set_ylabel(r'$M_{r0}$')
+    ax5.grid(linestyle='--')
+    ax5.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax6 setup
+    ax6.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax6.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax6.set_ylabel(r'$b_0$')
+    ax6.grid(linestyle='--')
+    ax6.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # ax7 setup
+    ax7.set_title(r'$r_{{mean}} = {0:.2} m$'.format(rMean))
+    ax7.set_xlabel(r'$\frac{V_{t0}}{U_{mean}}$')
+    ax7.set_ylabel(r'$r_{{tip}}$')
+    ax7.grid(linestyle='--')
+    ax7.legend(loc='upper left', bbox_to_anchor=[1,1])
+
+    # xticks setup 
+    ax0.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax1.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax2.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax3.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax4.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax5.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax6.set_xticks(np.arange(0.05, 0.4, 0.05))
+    ax7.set_xticks(np.arange(0.05, 0.4, 0.05))
+    
+    fig0.tight_layout()
+
+    plt.show()
+
+mFlux = 100     # mass flux                [kg/s]
+betaP = 1.45    # compression ratio        [--]
+maxD  = 0.9     # maximum tip diameter     [m]
+maxR  = maxD/2  # maximum tip radius       [m]
+rMean = 0.31
+Pt0 = 1e+5
+Tt0 = 300 
+stageStudy(mFlux, betaP, rMean, Pt0, Tt0)
